75500aa6cf71ae769351c4f2724da4a299dc4611
Peter Gemmell <peter.gemmell@mths.ca>
2022-02-24 19:55:44 +0000

    are & perimeter

---
+++ area_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# Created by Peter Gemmell
+# Created on February 2022
+# This is the Perimeter and Area for a rectangle of 5 x 3 dimensions
+
+
+def main():
+    # this function calculates the area and perimeter of a rectangle with the dimensions 3 by 5
+    print("If a Rectangle has the dimensions: ")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm2.".format(3 * 5))
+    print("Perimeter is {}cm.".format(2 * (3 + 5)))
+
+
+if __name__ == "__main__":
+    main()
